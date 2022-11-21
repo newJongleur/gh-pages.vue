c0d2c34764d203531ecebed056fa09ef62a78428
newJongleur <505755935@qq.com>
2022-11-21 10:44:22 +0800

    config update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '.',
+  publicPath: env !== "development" ? "/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
