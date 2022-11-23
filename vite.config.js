0e81af9dadaba624ab7857b5bdf653b90f370b7f
newJongleur <505755935@qq.com>
2022-11-23 13:47:53 +0800

    add router

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// https://cn.vitejs.dev/config/
 export default defineConfig({
   //publicPath: env !== "development" ? "/" : "/",
   base: './',
