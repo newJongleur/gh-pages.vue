64f73066f1a845f14cbbc9e02c0e7a725e1b1f44
newJongleur <505755935@qq.com>
2022-12-15 11:47:57 +0800

    Updates

---
+++ assets/404View.3210c774.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index.8e280a1e.js";const t={},n={class:"not-found"};function s(_,r){return c(),o("div",n," 404 ")}const f=e(t,[["render",s]]);export{f as default};
--- assets/404View.b8ce1c10.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index.4bdecfeb.js";const o={};function r(t,_){return c(),n("label",null,"404")}const s=e(o,[["render",r]]);export{s as default};
